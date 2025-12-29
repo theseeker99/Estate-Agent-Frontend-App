52a01c5beec56cd37a1a0eb85988d49a4fb1f4df
The Seeker <jalithapubgboss@gmail.com>
2025-12-29 14:10:32 +0530

    Added main images

--- src/App.jsx
+++ src/App.jsx
@@ -10,4 +10,6 @@ function App() {
 }
 
 // Favicon link: https://www.flaticon.com/free-icons/real-estate-agent
-export default App;
+// Images from free to use sites: https://pexels.com, https://unsplash.com
+
+    export default App;
